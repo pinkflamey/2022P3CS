402b1f153e334baf6abdc7e97e57f278fdcbfcda
pinkflamey <mavis@pinkflamey.nl>
2022-02-14 10:25:46 +0100

    first exercise

---
+++ les1/helloworld/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+
+        private string greeting;
+        private string greetWho;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            program.SetWhoToGreet("iedereen");
+            program.PrepareGreeting();
+
+            string greeting = program.GetGreeing();
+            Console.WriteLine(greeting);
+        }
+
+        private void SetWhoToGreet(string who)
+        {
+            greetWho = who;
+        }
+
+        private void PrepareGreeting()
+        {
+            greeting = "goedemorgen " + greetWho + "!";
+        }
+
+        private string GetGreeting()
+        {
+            return greeting;
+        }
+    }
+}
